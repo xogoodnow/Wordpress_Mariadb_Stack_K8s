07ada8bafd4368cd08feca7045c811bca1b322f8
xogoodnow <xo4ever9999@gmail.com>
2023-07-29 23:00:01 +0330

    Added 7 servers for k8s cluster (3Ma, 3Wo and 1Ha)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,59 @@
 
-resource "hcloud_server" "kangterraform" {
-  name         = "node1"
+resource "hcloud_server" "master-1" {
+  name         = "master-1"
   image        = var.image_name
-  server_type  = "cx11"
+  server_type  = "cpx31"
   ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id]
 
+}
+
+
+resource "hcloud_server" "master-2" {
+  name         = "master-2"
+  image        = var.image_name
+  server_type  = "cpx31"
+  ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id]
+
+}
+
+resource "hcloud_server" "master-3" {
+  name         = "master-3"
+  image        = var.image_name
+  server_type  = "cpx31"
+  ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id]
+
+}
+
+resource "hcloud_server" "worker-1" {
+  name         = "worker-1"
+  image        = var.image_name
+  server_type  = "cpx31"
+  ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id]
+
+}
+
+resource "hcloud_server" "worker-2" {
+  name         = "worker-2"
+  image        = var.image_name
+  server_type  = "cpx31"
+  ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id]
+
+}
+
+resource "hcloud_server" "worker-3" {
+  name         = "worker-3"
+  image        = var.image_name
+  server_type  = "cpx31"
+  ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id]
+
+}
+
+resource "hcloud_server" "haproxy" {
+  name         = "haproxy"
+  image        = var.image_name
+  server_type  = "cpx11"
+  ssh_keys = [data.hcloud_ssh_key.key1.id,data.hcloud_ssh_key.key2.id]
+  location = var.location
 }
 
 
