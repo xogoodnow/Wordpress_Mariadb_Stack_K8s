00a2724f51347e855745f86f74ed55ef77cd91b7
xogoodnow <xo4ever9999@gmail.com>
2023-07-29 23:10:20 +0330

    Added another flavor for Haproxy

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,9 +2,7 @@ variable "hcloud_token" {
   description = "The api key for hetzner"
   sensitive = true # Requires terraform >= 0.14
   type = string
-
 }
-
 
 variable "image_name" {
   description = "The image name for the server"
@@ -14,25 +12,19 @@ variable "image_name" {
     error_message = "The image name is not supported"
 
   }
-
 }
-
 
 variable "server_type" {
   description = "The type of server"
   type = string
   default = "cpx31"
   validation {
-    condition = contains(["cpx31"], var.server_type)
+    condition = contains(["cpx31", "cpx11"], var.server_type)
     #Additional servertypes will be added
     error_message = "Lowe than cpx31 would not be sufficient for the cluster"
 
   }
-
-
-
 }
-
 
 
 variable "location" {
@@ -44,10 +36,6 @@ variable "location" {
     error_message = "No other locations are supported"
     #Other location will be added
 
-
-
   }
-
-
 
 }
